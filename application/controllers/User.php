cb8bfcec01b57fcc9cefb57b6e6328c9f98968f5
BURGER Clément <clement.burger@gmail.com>
2018-03-07 19:28:19 +0100

    add form
    Ajout formulaire profil

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,19 +18,19 @@ class User extends BaseController
     {
         parent::__construct();
         $this->load->model('user_model');
-        $this->isLoggedIn();   
+        $this->isLoggedIn();
     }
-    
+
     /**
      * This function used to load the first screen of the user
      */
     public function index()
     {
         $this->global['pageTitle'] = 'Tableau de bord';
-        
+
         $this->loadViews("dashboard", $this->global, NULL , NULL);
     }
-    
+
     /**
      * This function is used to load the user list
      */
@@ -43,20 +43,20 @@ class User extends BaseController
         else
         {
             $this->load->model('user_model');
-        
+
             $searchText = $this->input->post('searchText');
             $data['searchText'] = $searchText;
-            
+
             $this->load->library('pagination');
-            
+
             $count = $this->user_model->userListingCount($searchText);
 
 			$returns = $this->paginationCompress ( "userListing/", $count, 5 );
-            
+
             $data['userRecords'] = $this->user_model->userListing($searchText, $returns["page"], $returns["segment"]);
-            
+
             $this->global['pageTitle'] = 'Liste des utilisateurs';
-            
+
             $this->loadViews("users", $this->global, $data, NULL);
         }
     }
@@ -74,7 +74,7 @@ class User extends BaseController
         {
             $this->load->model('user_model');
             $data['roles'] = $this->user_model->getUserRoles();
-            
+
             $this->global['pageTitle'] = 'Nouvel utilisateur';
 
             $this->loadViews("addNew", $this->global, $data, NULL);
@@ -98,7 +98,8 @@ class User extends BaseController
         if(empty($result)){ echo("true"); }
         else { echo("false"); }
     }
-    
+
+
     /**
      * This function is used to add new user to the system
      */
@@ -111,14 +112,14 @@ class User extends BaseController
         else
         {
             $this->load->library('form_validation');
-            
+
             $this->form_validation->set_rules('fname','Full Name','trim|required|max_length[128]|xss_clean');
             $this->form_validation->set_rules('email','Email','trim|required|valid_email|xss_clean|max_length[128]');
             $this->form_validation->set_rules('password','Password','required|max_length[20]');
             $this->form_validation->set_rules('cpassword','Confirm Password','trim|required|matches[password]|max_length[20]');
             $this->form_validation->set_rules('role','Role','trim|required|numeric');
             $this->form_validation->set_rules('mobile','Mobile Number','required|min_length[10]|xss_clean');
-            
+
             if($this->form_validation->run() == FALSE)
             {
                 $this->addNew();
@@ -130,13 +131,13 @@ class User extends BaseController
                 $password = $this->input->post('password');
                 $roleId = $this->input->post('role');
                 $mobile = $this->input->post('mobile');
-                
+
                 $userInfo = array('email'=>$email, 'password'=>getHashedPassword($password), 'roleId'=>$roleId, 'name'=> $name,
                                     'mobile'=>$mobile, 'createdBy'=>$this->vendorId, 'createdDtm'=>date('Y-m-d H:i:s'));
-                
+
                 $this->load->model('user_model');
                 $result = $this->user_model->addNewUser($userInfo);
-                
+
                 if($result > 0)
                 {
                     $this->session->set_flashdata('success', 'Création de l\'utilisateur réussie');
@@ -145,13 +146,92 @@ class User extends BaseController
                 {
                     $this->session->set_flashdata('error', 'La création de l\'utilisateur a échouée');
                 }
-                
+
                 redirect('addNew');
             }
         }
     }
 
-    
+    /**
+     * This function is used to load the add new form
+     */
+    function addProfil()
+    {
+        if($this->isAdmin() == TRUE)
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->load->model('user_model');
+            $data['roles'] = $this->user_model->getUserRoles();
+
+            $this->global['pageTitle'] = 'Nouveau Profil';
+
+            $this->loadViews("addProfil", $this->global, $data, NULL);
+        }
+    }
+
+    /**
+     * This function is used to add new user profil to the system
+     */
+    function addNewProfil()
+    {
+        if($this->isAdmin() == TRUE)
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_rules('fname','First Name','trim|required|max_length[128]|xss_clean');
+            $this->form_validation->set_rules('lname','Last Name','trim|required|max_length[128]|xss_clean');
+            $this->form_validation->set_rules('email','Email','trim|required|valid_email|xss_clean|max_length[128]');
+            $this->form_validation->set_rules('mobile','Mobile Number','required|min_length[10]|xss_clean');
+            $this->form_validation->set_rules('fixe','Mobile Number','required|min_length[10]|xss_clean');
+            $this->form_validation->set_rules('cpostal','Code Postal','required|min_length[5]|xss_clean');
+            $this->form_validation->set_rules('adresse','Adresse','trim|required|max_length[256]|xss_clean');
+            $this->form_validation->set_rules('city','Ville','trim|required|max_length[128]|xss_clean');
+            $this->form_validation->set_rules('country','Pay','trim|required|max_length[128]|xss_clean');
+
+
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->addProfil();
+            }
+            else
+            {
+              /*
+                $name = ucwords(strtolower($this->input->post('fname')));
+                $email = $this->input->post('email');
+                $password = $this->input->post('password');
+                $roleId = $this->input->post('role');
+                $mobile = $this->input->post('mobile');
+
+                $userInfo = array('email'=>$email, 'password'=>getHashedPassword($password), 'roleId'=>$roleId, 'name'=> $name,
+                                    'mobile'=>$mobile, 'createdBy'=>$this->vendorId, 'createdDtm'=>date('Y-m-d H:i:s'));
+
+                $this->load->model('user_model');
+                */
+                $result = 1;//$this->user_model->addNewUser($userInfo);
+
+
+                if($result > 0)
+                {
+                    $this->session->set_flashdata('success', 'Création du profil réussie');
+                }
+                else
+                {
+                    $this->session->set_flashdata('error', 'La création du profil a échouée');
+                }
+
+                redirect('addProfil');
+            }
+        }
+    }
+
+
     /**
      * This function is used load user edit information
      * @param number $userId : Optional : This is user id
@@ -168,17 +248,17 @@ class User extends BaseController
             {
                 redirect('userListing');
             }
-            
+
             $data['roles'] = $this->user_model->getUserRoles();
             $data['userInfo'] = $this->user_model->getUserInfo($userId);
-            
+
             $this->global['pageTitle'] = 'Profil utilisateur';
-            
+
             $this->loadViews("editOld", $this->global, $data, NULL);
         }
     }
-    
-    
+
+
     /**
      * This function is used to edit the user information
      */
@@ -191,16 +271,16 @@ class User extends BaseController
         else
         {
             $this->load->library('form_validation');
-            
+
             $userId = $this->input->post('userId');
-            
+
             $this->form_validation->set_rules('fname','Full Name','trim|required|max_length[128]|xss_clean');
             $this->form_validation->set_rules('email','Email','trim|required|valid_email|xss_clean|max_length[128]');
             $this->form_validation->set_rules('password','Password','matches[cpassword]|max_length[20]');
             $this->form_validation->set_rules('cpassword','Confirm Password','matches[password]|max_length[20]');
             $this->form_validation->set_rules('role','Role','trim|required|numeric');
             $this->form_validation->set_rules('mobile','Mobile Number','required|min_length[10]|xss_clean');
-            
+
             if($this->form_validation->run() == FALSE)
             {
                 $this->editOld($userId);
@@ -212,9 +292,9 @@ class User extends BaseController
                 $password = $this->input->post('password');
                 $roleId = $this->input->post('role');
                 $mobile = $this->input->post('mobile');
-                
+
                 $userInfo = array();
-                
+
                 if(empty($password))
                 {
                     $userInfo = array('email'=>$email, 'roleId'=>$roleId, 'name'=>$name,
@@ -223,12 +303,12 @@ class User extends BaseController
                 else
                 {
                     $userInfo = array('email'=>$email, 'password'=>getHashedPassword($password), 'roleId'=>$roleId,
-                        'name'=>ucwords($name), 'mobile'=>$mobile, 'updatedBy'=>$this->vendorId, 
+                        'name'=>ucwords($name), 'mobile'=>$mobile, 'updatedBy'=>$this->vendorId,
                         'updatedDtm'=>date('Y-m-d H:i:s'));
                 }
-                
+
                 $result = $this->user_model->editUser($userInfo, $userId);
-                
+
                 if($result == true)
                 {
                     $this->session->set_flashdata('success', 'Mise à jour effectuée');
@@ -237,7 +317,7 @@ class User extends BaseController
                 {
                     $this->session->set_flashdata('error', 'La mise à jour a échoué');
                 }
-                
+
                 redirect('userListing');
             }
         }
@@ -258,36 +338,36 @@ class User extends BaseController
         {
             $userId = $this->input->post('userId');
             $userInfo = array('isDeleted'=>1,'updatedBy'=>$this->vendorId, 'updatedDtm'=>date('Y-m-d H:i:s'));
-            
+
             $result = $this->user_model->deleteUser($userId, $userInfo);
-            
+
             if ($result > 0) { echo(json_encode(array('status'=>TRUE))); }
             else { echo(json_encode(array('status'=>FALSE))); }
         }
     }
-    
+
     /**
      * This function is used to load the change password screen
      */
     function loadChangePass()
     {
         $this->global['pageTitle'] = 'Changer de mot de passe';
-        
+
         $this->loadViews("changePassword", $this->global, NULL, NULL);
     }
-    
-    
+
+
     /**
      * This function is used to change the password of the user
      */
     function changePassword()
     {
         $this->load->library('form_validation');
-        
+
         $this->form_validation->set_rules('oldPassword','Ancien mot de passe','required|max_length[20]');
         $this->form_validation->set_rules('newPassword','Nouveau mot de passe','required|max_length[20]');
         $this->form_validation->set_rules('cNewPassword','Confirmez votre mot de passe','required|matches[newPassword]|max_length[20]');
-        
+
         if($this->form_validation->run() == FALSE)
         {
             $this->loadChangePass();
@@ -296,9 +376,9 @@ class User extends BaseController
         {
             $oldPassword = $this->input->post('oldPassword');
             $newPassword = $this->input->post('newPassword');
-            
+
             $resultPas = $this->user_model->matchOldPassword($this->vendorId, $oldPassword);
-            
+
             if(empty($resultPas))
             {
                 $this->session->set_flashdata('nomatch', 'Votre ancien mot de passe n\'est pas correct');
@@ -308,12 +388,12 @@ class User extends BaseController
             {
                 $usersData = array('password'=>getHashedPassword($newPassword), 'updatedBy'=>$this->vendorId,
                                 'updatedDtm'=>date('Y-m-d H:i:s'));
-                
+
                 $result = $this->user_model->changePassword($this->vendorId, $usersData);
-                
+
                 if($result > 0) { $this->session->set_flashdata('success', 'Mise à jour du mot de passe effectuée'); }
                 else { $this->session->set_flashdata('error', 'La mise à jour du mot de passe a échouée'); }
-                
+
                 redirect('loadChangePass');
             }
         }
@@ -322,7 +402,7 @@ class User extends BaseController
     function pageNotFound()
     {
         $this->global['pageTitle'] = ' 404 - Page non trouvée';
-        
+
         $this->loadViews("404", $this->global, NULL, NULL);
     }
 }
